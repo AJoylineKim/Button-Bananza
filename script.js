ad737493fe27edaf8a18a2a915a43cd6794f3815
AJoylineKim <akimberly@rloacs.org>
2026-09-09 13:35:29 -0400

    Update body styles in script.js for background image

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-{ background-image:url('nyancat.gif'); background-repeat:repeat; }
+body{
+    background-image: url('nyancat.gif');
+    background-size: cover;
+    
+    
+    
+    height: 100vh;
+    padding:0;
+    margin:0;
+}
