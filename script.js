d760fc6b062418ac633c3bfbd41bb0e49d893675
AJoylineKim <akimberly@rloacs.org>
2026-09-10 08:33:09 -0400

    Fix syntax error in redbutton click event listener

--- script.js
+++ script.js
@@ -2,4 +2,4 @@ redbutton.addEventListener ("click", (function) {
       let backgroundi = Math.floor(Math.random() * 10);
   function myFunction() {
     document.getElementById("redbutton").style.backgroundImage = "nyancat.gif";
-}
+}));
