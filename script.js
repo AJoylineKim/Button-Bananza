0db8fee0a658747d7190c34c43f10c9e8df521e0
AJoylineKim <akimberly@rloacs.org>
2026-09-10 09:31:07 -0400

    Fix image style application on click event

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const redbutton = document.getElementById (redbutton);
 
 redbutton.addEventListener ("click", function() { 
-    document.getElementById("div").style.Image = "url(nyancat.gif)";
+    document.getElementById("img").style.Image = "url(nyancat.gif)";
 });
