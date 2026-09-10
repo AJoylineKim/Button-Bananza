f12b9c84a978a2b1bf8705d1f2495adacba6c37e
AJoylineKim <akimberly@rloacs.org>
2026-09-10 08:52:23 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-redbutton.addEventListener ("click", (function) { 
-      let backgroundi = Math.floor(Math.random() * 10);
-  function myFunction() {
-    document.getElementById("redbutton").style.backgroundImage = "nyancat.gif";
-}));
