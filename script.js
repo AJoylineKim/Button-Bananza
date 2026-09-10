c6731d64da70039276fed974706ae448a0bcef29
AJoylineKim <akimberly@rloacs.org>
2026-09-10 07:48:19 -0400

    Implement click event for red button
    Add event listener to red button to change background image.

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+redbutton.addEventListener ("click", (event) => { 
+      let backgroundi = Math.floor(Math.random() * 10);
+  function myFunction() {
+    document.getElementById("myDIV").style.backgroundImage = "nyancat.gif";
+}
