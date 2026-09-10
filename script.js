1ac9818f0670300d853451ae3d33100866b801c1
AJoylineKim <akimberly@rloacs.org>
2026-09-10 09:06:56 -0400

    Update background image URL on button click

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 redbutton.addEventListener ("click", function() { 
-    document.getElementById("body").style.backgroundImage = "url(nyancat.gif)";
+    document.getElementById("body").style.backgroundImage = "url(https://pin.it/7iALOkSRU)";
 });
