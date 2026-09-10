433bd9c78e015dd762074dbaaf201dfa1bf467f9
AJoylineKim <akimberly@rloacs.org>
2026-09-10 09:14:34 -0400

    Change background image on button click

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 redbutton.addEventListener ("click", function() { 
-    document.getElementById("body").style.backgroundImage = "url(https://pin.it/7iALOkSRU)";
+    document.getElementById("body").style.backgroundImage = "url(nyancat.gif)";
 });
