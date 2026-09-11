aa3f157f9b0d41b70cd125a94144d9af13aa9aa4
AJoylineKim <akimberly@rloacs.org>
2026-09-11 08:25:04 -0400

    Fix image attributes and add button click event
    Added event listener for 'redbutton' to log a message.

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const container = document.getElementById('imgcontainer');
 
 button.addEventListener('click', function() {
   const img = document.createElement('img');
-  img.src = 'nyancat.gif'; 
-  img.alt = 'nyancat.gif';
+  img.src = "nyancat.gif"; 
+  img.alt = "nyancat.gif";
   container.appendChild(img);
 });
 
+document.getElementById("redbutton").addEventListener("click", function() {
+    document.getElementById("consoleLog").textContent = "You clicked the button!";
+});
