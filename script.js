479bbe55ed35449b1e0b1b25fee0bf127e85a888
AJoylineKim <akimberly@rloacs.org>
2026-09-11 07:43:05 -0400

    Change button ID and append image on click

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const button = document.getElementById('button');
+const button = document.getElementById('redbutton');
 const container = document.getElementById('imgcontainer');
 
-
 button.addEventListener('click', function() {
-  
-    const img = document.createElement('img');
-    img.src = 'nyancat.gif'; 
-    img.alt = 'nyancat.gif';
+  const img = document.createElement('img');
+  img.src = 'nyancat.gif'; 
+  img.alt = 'nyancat.gif';
+  container.appendChild(img);
+});
